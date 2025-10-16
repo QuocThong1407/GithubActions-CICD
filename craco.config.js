35097406f125d5950502a82977eb77fc4d190a8f
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2025-10-16 21:08:24 +0700

    fix: Add setup file for jest-allure

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,8 @@ module.exports = {
         'default',
         'jest-allure'
       ],
-      testRunner: 'jest-jasmine2'
+      testRunner: 'jest-jasmine2',
+      setupFilesAfterEnv: ['<rootDir>/src/allure-setup.js']
     }
   }
 };
